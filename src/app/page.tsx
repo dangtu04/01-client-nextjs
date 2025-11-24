cce872a2d5014f15aa4f5c8087a7ab5d8a3686d4
DangThanhTu <dangtu22304@gmail.com>
2025-11-24 23:14:28 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      Hello world
+      <div>
+        <Button type="primary">Click</Button>
+      </div>
+    </div>
+  );
+}
